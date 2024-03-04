6c48b709e6805eb8ecc45c549bea392fb46ca33e
xltham <guilhermesalvadormaia@gmail.com>
2024-03-04 13:41:06 -0400

    responsividade do layout

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -3,7 +3,7 @@ import { LayoutBasedePagina } from '../../shared/layouts';
 
 export const Dashboard = () => {
   return(
-    <LayoutBasedePagina titulo='pagina inicial'>
+    <LayoutBasedePagina titulo='pagina inicial' barraDeFerramentas={<>barra de Ferramenta</>}>
      testando
     </LayoutBasedePagina>
   );
--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -1,37 +1,48 @@
 import { Box, Icon, IconButton, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { useDrawerContext } from '../contexts';
+import { ReactNode } from 'react';
 
 
 interface ILayoutBaseDePaginaProps {
   children?: React.ReactNode;
   titulo:string
+  barraDeFerramentas?: ReactNode
 }
 
-export const LayoutBasedePagina: React.FC<ILayoutBaseDePaginaProps> = ({children, titulo}) => {
+export const LayoutBasedePagina: React.FC<ILayoutBaseDePaginaProps> = ({children, titulo, barraDeFerramentas}) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'));
 
   const {toggleDrawerOpen}= useDrawerContext();
   return(
     <Box height={'100%'} display='flex' flexDirection='column' gap={1}>
-      <Box padding={1} display='flex' alignItems='center' height={theme.spacing(12)}>
+      <Box padding={1} display='flex' alignItems='center' height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}>
         {smDown && (
           <IconButton onClick={toggleDrawerOpen}>
             <Icon>menu</Icon>
           </IconButton>
         )}
        
-        <Typography variant='h5'>
+        <Typography 
+          overflow='hidden'
+          whiteSpace='nowrap'
+          textOverflow='ellipsis'
+          variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+        >
+
           {titulo}
         </Typography>
 
       </Box>
 
-      <Box>
-        Barra de ferramentas
-      </Box>
+      {barraDeFerramentas && (
+        <Box>
+          {barraDeFerramentas}
+        </Box> 
+      )}
 
-      <Box>
+      <Box flex={1} overflow='auto'>
         {children}
       </Box>
     </Box>
